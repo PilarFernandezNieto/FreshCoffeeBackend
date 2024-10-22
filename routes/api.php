46a5237153298566a2637e548a978817c831f9f8
Pilar Fernández Nieto <pilarfnieto@gmail.com>
2024-10-22 20:38:57 +0200

    Categorias

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    // Aquí se colocan todas las rutas que requieran autenticación
     return $request->user();
+
 });
+
+// apiResource asocia los nombres de las funciones de los controladores
+Route::apiResource('/categorias', CategoriaController::class);
